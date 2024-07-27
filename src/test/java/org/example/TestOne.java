f0055c23da969496449f89e5ea979c52074abd28
jeeva <jeepadips15@gmail.com>
2024-07-27 13:03:56 -0300

    second successful commit

---
+++ src/test/java/org/example/TestOne.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class TestOne {
+    WebDriver driver;
+    @BeforeSuite
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testOne() throws InterruptedException {
+        driver.get("https://www.google.com");
+        Assert.assertEquals("Google",driver.getTitle());
+
+        Thread.sleep(4000);
+
+        driver.quit();
+    }
+}
